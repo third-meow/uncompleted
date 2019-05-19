7022cfb265588dc6aa6ed46565f0f85d032f63f0
The third meow <jsheaandcats@gmail.com>
2019-05-20 10:30:04 +1200

    build simple completion selector

--- src/bin/__uncompleted_shell_completion.rs
+++ src/bin/__uncompleted_shell_completion.rs
@@ -1,5 +1,13 @@
 
 
 fn main() {
-    println!("complete");
+    let word_list = vec!["you've", "been", "completed"];
+    let args : Vec<String> = std::env::args().collect();
+    let to_complete : &String = &args[2];
+
+    for word in word_list {
+        if word.starts_with(to_complete) {
+            println!("{}", word);
+        }
+    }
 }
